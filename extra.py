f2e3445deb574d947c1dd678b09807e2c868bdaf
deepu1067 <dshahadat10@gmail.com>
2023-08-30 05:42:43 +0600

    interface for prediction

---
+++ extra.py
@@ -0,0 +1,35 @@
+classes_plant = {0: "tomato", 1: "potato", 2: "corn", 3: "grape"}
+grape = {
+    0: "black_measles",
+    1: "leaf_blight_(isariopsis_leaf_spot)",
+    2: "black_rot",
+    3: "healthy",
+}
+corn = {0: "healthy", 1: "common_rust", 2: "gray_leaf_spot", 3: "northern_leaf_blight"}
+tomato = {
+    0: "septoria_leaf_spot",
+    1: "healthy",
+    2: "early_blight",
+    3: "yellow_leaf_curl_virus",
+    4: "target_spot",
+    5: "bacterial_spot",
+    6: "late_blight",
+    7: "spider_mites_(two_spotted_spider_mite)",
+    8: "leaf_mold",
+    9: "mosaic_virus",
+}
+potato = {0: "healthy", 1: "early_blight", 2: "late_blight"}
+
+
+def get_plant(plant):
+    return classes_plant[plant]
+
+def get_model(plant):
+    if plant == 0:
+        return 'models/tomate.h5' , tomato
+    if plant == 1:
+        return 'models/potato.h5', potato
+    if plant == 2:
+        return 'models/corn.h5' , corn
+    if plant == 4:
+        return 'models/grape.h5' , grape
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 from flask import Flask, redirect , render_template , request 
-import subprocess , re
+import subprocess , re , os
+from extra import *
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import cv2
+import numpy as np
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'img')
+plant_model = load_model('models\plant_model.h5')
 
 
 @app.route("/", methods=['GET' , 'POST'])
 def main():
+    title = ""
+    image = ""
     if request.method == 'POST':
-        pass
-    return render_template('index.html')
+        if 'image' in request.files:
+            uploaded_image = request.files['image']
+            if uploaded_image.filename != '':
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_image.filename)
+                uploaded_image.save(image_path)
+                
+                #preprocessing
+                images = cv2.imread(image_path, cv2.IMREAD_COLOR)
+                images = cv2.resize(images, (240,240))
+                # Make predictions using the model
+                plant_predictions = plant_model.predict(np.array([images]))
+                plant_pred = np.argmax(plant_predictions)
 
-@app.route("/result")
+                d_model_path, classes = get_model(plant_pred)
+                disease_model = load_model(d_model_path)
+                disease_pred = disease_model.predict(np.array([images]))
+                pred = np.argmax(disease_pred)
+
+                title =  get_plant(plant_pred) + " " + classes[pred]
+                image = uploaded_image.filename
+
+            
+        
+    return render_template('index.html', title = title , image = image)
+
 
 def get_address():
     output = subprocess.check_output('ipconfig', shell=True, text=True)
